8a930852401ada1bbd03906be0a7085830c9d541
Guido Dizioli <guidodizi@hotmail.com>
2017-05-06 20:52:54 -0300

    regresion

--- Neuronal Network.py
+++ Neuronal Network.py
@@ -1,17 +1,13 @@
-from math import exp
+import math
 from random import seed
 from random import random
 
 # Initialize a network
-def initialize_network(n_inputs, n_hidden, expected_outputs):
+def initialize_network(n_inputs, n_hidden):
 	network = list()
 	hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
 	network.append(hidden_layer)
-	output_layer = []
-	for i in range(len(expected_outputs)):
-		return_value = expected_outputs[i]
-		weights = [random() for i in range(n_hidden + 1)]
-		output_layer.append({'weights': weights, 'retrun_value': return_value})
+	output_layer = [{'weights':[random() for i in range(n_hidden + 1)]}]
 	network.append(output_layer)
 	return network
 
@@ -24,7 +20,7 @@ def activate(weights, inputs):
 
 # Transfer neuron activation
 def transfer(activation):
-	return 1.0 / (1.0 + exp(-activation))
+	return 1.0 / (1.0 + math.exp(-activation))
 
 # Forward propagate input to a network output
 def forward_propagate(network, row):
@@ -33,10 +29,11 @@ def forward_propagate(network, row):
 		new_inputs = []
 		for neuron in layer:
 			activation = activate(neuron['weights'], inputs)
-			neuron['output'] = transfer(activation)
+			# neuron['output'] = transfer(activation)
+			neuron['output'] = math.tanh(activation)
 			new_inputs.append(neuron['output'])
 		inputs = new_inputs
-	return inputs
+	return inputs[0]
 
 # Calculate the derivative of an neuron output
 def transfer_derivative(output):
@@ -56,7 +53,7 @@ def backward_propagate_error(network, expected):
 		else:
 			for j in range(len(layer)):
 				neuron = layer[j]
-				errors.append(expected[j] - neuron['output'])
+				errors.append(expected - neuron['output'])
 		for j in range(len(layer)):
 			neuron = layer[j]
 			neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
@@ -89,47 +86,33 @@ def firstFunction (x):
 	return (x**3) - (x**2) + 1 
 
 # Train a network for a fixed number of epochs
-def train_network(network, train, l_rate, n_epoch, expected_outputs):
+def train_network(network, train, l_rate, n_epoch):
 	for epoch in range(n_epoch):
 		sum_error = 0
 		for row in train:
-			outputs = forward_propagate(network, row)
-			expected = [0 for i in range(len(expected_outputs))]
-			expected[expected_outputs.index(row[-1])] = 1
-		 	sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
-			backward_propagate_error(network, expected)
+			output = forward_propagate(network, row)
+		 	sum_error += (row[-1]-output)**2
+			backward_propagate_error(network, row[-1])
 			update_weights(network, row, l_rate)
-		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error/2))
 
 # Make a prediction with a network
-def predict(network, row, expected_outputs):
-	outputs = forward_propagate(network, row)
-	index_output = outputs.index(max(outputs))
-	return expected_outputs[index_output]
+def predict(network, row):
+	return forward_propagate(network, row)
+
 
 # Test training backprop algorithm
 seed(1)
 dataset = createDataset(firstFunction, 40, -1, 1)
-# dataset = [[2.7810836,2.550537003,0],
-# 	[1.465489372,2.362125076,0],
-# 	[3.396561688,4.400293529,0],
-# 	[1.38807019,1.850220317,0],
-# 	[3.06407232,3.005305973,0],
-# 	[7.627531214,2.759262235,1],
-# 	[5.332441248,2.088626775,1],
-# 	[6.922596716,1.77106367,1],
-# 	[8.675418651,-0.242068655,1],
-# 	[7.673756466,3.508563011,1]]
+
 #number of variables on input
 n_inputs = len(dataset[0]) - 1
-# #Number of possible output values
-# n_outputs = len(set([row[-1] for row in dataset]))
-expected_outputs = [row[-1] for row in dataset]
-network = initialize_network(n_inputs, 2, expected_outputs)
-train_network(network, dataset, 0.5, 100000, expected_outputs)
+
+network = initialize_network(n_inputs, 2)
+train_network(network, dataset, 0.5, 100)
 
 for row in dataset:
-	prediction = predict(network, row, expected_outputs)
+	prediction = predict(network, row)
 	print('Expected=%.5f, Got=%.5f' % (row[-1], prediction))
 
 # for layer in network:
