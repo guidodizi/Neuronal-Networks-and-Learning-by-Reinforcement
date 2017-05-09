bd4035ed40af5b19a84d654fce58cef2e0e4cb5f
Nicolas <nicolascrocco@gmail.com>
2017-05-08 22:03:48 -0300

    fixes q learner

--- Q Learning.py
+++ Q Learning.py
@@ -2,13 +2,17 @@ from random import randint
 import time
  
 
-EPISODES = 1
+EPISODES = 30
 
 class Qlearning:
 
-	def __init__(self):
-		self.gamma = 0.9 
-
+	def __init__(self,part):
+		
+		if part == 0:
+			self.gamma = 0.9 
+		elif part == 1:
+			self.gamma = 0.4
+		
 		self.states = []
 		for i in range(0,36):
 			self.states.append(i)
@@ -16,20 +20,23 @@ class Qlearning:
 		self.statesCount = 36
 	 
 		self.R = [[0 for x in range(self.statesCount)] for y in range(self.statesCount)]  # reward lookup
-		self.Q = [[0 for x in range(self.statesCount)] for y in range(self.statesCount)]  # Q learning
-		#self.Q = [[randint(-50,50) for x in range(self.statesCount)] for y in range(self.statesCount)]  # Q learning
+		if part == 0:
+			self.Q = [[0 for x in range(self.statesCount)] for y in range(self.statesCount)]  # Q learning
+		elif part == 1:
+			self.Q = [[randint(-50,50) for x in range(self.statesCount)] for y in range(self.statesCount)]  # Q learning
 
 		self.actions = []
 		for i in range(0,36):
 			actionsFrom = []
-			if i % 5 != 0: # si no esta en la columna de mas a la derecha agrego el de su der
+			if i % 6 != 5: # si no esta en la columna de mas a la derecha agrego el de su der
 				actionsFrom.append(i+1) 
 			if i % 6 != 0: # si no esta en la columna de mas a la izq agrego el de su izq
 				actionsFrom.append(i-1) 
 			if i > 5: # si no esta en la fila de mas arriba agrego el de arriba
 				actionsFrom.append(i-6) 
 			if i < 30: # si no esta en la fila de mas abajo agrego el de abajo
-				actionsFrom.append(i+6)		
+				actionsFrom.append(i+6)	
+
 			self.actions.append(actionsFrom)
 
 
@@ -42,62 +49,56 @@ class Qlearning:
 
 		self.statesGoal = [13,14,19,20]
 		
-		self.epsilon = float(1) / EPISODES
+		self.epsilon = float(1) / (EPISODES + (EPISODES*.4))
 
 	def run(self): 
-	# 1. Set parameter , and environment reward matrix R 
-	# 2. Initialize matrix Q as zero matrix 
-	# 3. For each episode: Select random initial state 
-	#	Do while not reach goal state o 
-	#		Select one among all possible actions for the current state o 
-	#		Using this possible action, consider to go to the next state o 
-	#		Get maximum Q value of this next state based on all possible actions o 
-	#		Compute o Set the next state as the current state
 		for i in range(0,EPISODES):
+			#print "EPISODE: " + str(i)
 			stack = []
-			#Select random initial state
-			state = randint(0,self.statesCount-1)		
-			while state not in self.statesGoal:
+
+			#Select random initial state different to the goal state
+			while True:
+				state = randint(0,self.statesCount-1)	
+				keepSearching = state in self.statesGoal
+				if keepSearching:
+					continue 			
+				else:
+					break
 			
+			while not keepSearching:		
 				#Obtengo las acciones del estado actual
 				actionsFromState = self.actions[state]	
-				rand = float(randint(0,9))/ 10			
-						
+
+				#For each state decide if its exploration or explotation
+				rand = float(randint(0,99))/ 100			
 				if (rand > self.epsilon):
 					#EXPLORACION: Selecciono una de las acciones posibles en forma aleatoria				
 					index = randint(0,len(actionsFromState)-1)			
-					action = actionsFromState[index]				
+					action = actionsFromState[index]
 				else:
 					#EXPLOTACION: Selecciono la mejor accion posible									
 					action = self.max(state)
-
 				nextState = action
-				#Using this possible action, consider to go to the next state
-				q = self.Q[state][action]
-				maxQ = self.maxQ(nextState)
-				r = self.R[state][action]
 
-
-				#value = q + self.alpha * (r + self.gamma * maxQ - q)
-				value = r + self.gamma*(maxQ)
+				stack.append({'state':state,'nextState':nextState,'action':action})				
 				
-				print "estoy en estado: " + str(state)
-				print "accion: " + str(action)
-				print "next state: " + str(nextState)
-				print "q : " + str(q) + " maxQ: " + str(maxQ) + " r: " + str(r)
-				print "value: " + str(value)
-				print "----------"
-
-				stack.append({'state':state,'value':value,'action':action})
-				#self.Q[state][action] =  value
 				#Set the next state as the current state
 				state = nextState
-
+				keepSearching = state in self.statesGoal
+				
 			#recorro en orden inverso el stack y actualizo el Q		
 			while len(stack) > 0:
-				item = stack.pop()		
-				self.Q[item['state']][item['action']] = item['value']
-			self.epsilon += float(1) / EPISODES
+				item = stack.pop()
+
+				q = self.Q[item['state']][item['action']]
+				maxQ = self.maxQ(item['nextState'])
+				r = self.R[item['state']][item['action']]
+				
+				value = r + self.gamma*(maxQ)
+				if q < value:
+					self.Q[item['state']][item['action']] = value
+				
+			self.epsilon += float(1) / (EPISODES + (EPISODES*.4))							
 
 	def max (self,s):
 		actionsFromState = self.actions[s]
@@ -133,15 +134,14 @@ class Qlearning:
 	def policy (self,state):
 		actionsFromState = self.actions[state]
 		maxValue = -10000
-		policiGoToState = state
+		policyGoToState = state
 		for i in range (0,len(actionsFromState)):
 			nextState = actionsFromState[i]
-			value = self.Q[state][nextState]
-			
+			value = self.Q[state][nextState]			
 			if (value > maxValue):
 				maxValue = value
-				policiGoToState = nextState
-		return policiGoToState
+				policyGoToState = nextState
+		return policyGoToState
 		
 	def printResult(self):
 		print "Print result"
@@ -151,18 +151,24 @@ class Qlearning:
 				stateN += str(self.Q[i][j]) + " "
 			print "out from " + str(self.states[i]) + ": " + stateN
 	
-	# policy is maxQ(states)
 	def showPolicy(self):
 		print("\nshowPolicy")
 		for i in range(0,len(self.states)):
 			sfrom = self.states[i]
 			sto =  self.policy(sfrom)
-			print("from "+ str(self.states[sfrom]) + " goto "+ str(self.states[sto]))
-                 
-
-
+			print("from "+ str(self.states[sfrom]) + " goto "+ str(self.states[sto]) + " value " + str(self.Q[sfrom][sto]))
+                
 ##MAIN
-obj = Qlearning() 
-obj.run()
-obj.printResult()
-obj.showPolicy()
+
+while True:
+	part = input("Choose '0' for part B or '1' for part C:  ")
+	if part == 0 or part == 1:
+		break
+	else:
+		print "\nSorry, invalid input\n"
+		continue
+
+qLearner = Qlearning(part) 
+qLearner.run()
+qLearner.printResult()
+qLearner.showPolicy()
